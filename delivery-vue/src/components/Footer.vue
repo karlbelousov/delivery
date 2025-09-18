2c3182fd8c2d80cbc696c7f756e89980dd3d3e88
Karl Belousov <belousovkarl@gmail.com>
2025-09-18 12:09:53 +0600

    Урок 5. Vue.js

---
+++ delivery-vue/src/components/Footer.vue
@@ -0,0 +1,87 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="footer-box">
+        <router-link to="/" class="footer-logo logo">
+          <img src="../assets/images/logo.png" alt="logo" />
+        </router-link>
+        <div class="footer-nav">
+          <ul>
+            <li>
+              <router-link to="/rest">Ресторанам</router-link>
+            </li>
+            <li>
+              <router-link to="/rest">Курьерам</router-link>
+            </li>
+            <li>
+              <router-link to="/rest">Пресс-центр</router-link>
+            </li>
+            <li>
+              <router-link to="/rest">Контакты</router-link>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-social">
+          <img src="../assets/images/social/INST.png" alt="INST" />
+          <img src="../assets/images/social/FB.png" alt="FB" />
+          <img src="../assets/images/social/VK.png" alt="VK" />
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+ .footer {
+    min-height: 156px;
+    display: flex;
+    align-items: center;
+    padding: 60px 0;
+  }
+  
+  .footer-box {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 36px;
+    flex-wrap: wrap;
+  }
+  
+  .footer-nav {
+    flex-grow: 1;
+  }
+  
+  .footer-nav ul {
+    display: flex;
+    align-items: center;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    gap: 15px;
+    flex-wrap: wrap;
+  }
+  
+  .footer-nav ul li {
+    color: #595959;
+    font-size: 18px;
+    font-weight: 400;
+  }
+  
+  .footer-nav ul li a {
+    text-decoration: none;
+    color: inherit;
+  }
+  
+  .footer-social {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 20px;
+  }
+  
+</style>
---
+++ delivery-vue/src/views/ProductView.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="products">
+    <div class="container">
+      <div class="products-wrapper" id="goods-container">
+        <div class="product-card" v-for="good in goodArray" :key="good.id">
+          <div class="product-card__image">
+            <img :src="getUrl(good.image)" alt="goods-1" />
+          </div>
+          <div class="product-card__description">
+            <div class="product-card__description-row">
+              <h5 class="product-card__description--name">
+                {{ good.title }}
+              </h5>
+            </div>
+            <div class="product-card__description-row">
+              <p class="product-card__description--text">
+                {{ good.description }}
+              </p>
+            </div>
+            <div class="product-card__description-row">
+              <div class="product-card__description-controls">
+                <button class="btn btn-primary">
+                  В корзину
+                  <img
+                    src="../assets/images/icons/shopping-cart-white.svg"
+                    alt="cart"
+                  />
+                </button>
+                <span class="product-card__description-controls--price">
+                 {{ good.price }}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { goodArray } from '@/constants/goods';
+const getUrl = (name) => {
+  return new URL(`../assets/images/goods/${name}`, import.meta.url);
+}
+</script>
